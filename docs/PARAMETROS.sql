8f5f5e39d6e22a40db175afc2abb135311a9d6c2
Welington Ferreira <welington.kleber@gmail.com>
2025-03-24 15:49:06 -0300

    script

--- docs/PARAMETROS.sql
+++ docs/PARAMETROS.sql
@@ -1,4 +1,4 @@
 CREATE TABLE parametros (
     NomeRelatorio VARCHAR(100) NOT NULL, -- Nome do relatório
-    Caminho VARCHAR(255) NOT NULL       -- Caminho do relatório
+    CaminhoRelatorio VARCHAR(255) NOT NULL       -- Caminho do relatório
 );
